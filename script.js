8be490c8a25336d68f2df78f2ee5522c6b5b3d9d
Almando <almandocode@gmail.com>
2022-09-24 05:45:13 +0200

    my initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const containerEl = document.querySelector(".container");
+
+
+for (let index = 0; index < 30; index++) {
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("color-container")
+    containerEl.append(colorContainerEl)
+    
+}
+const colorContainerEls = document.querySelectorAll(".color-container")
+generateColors();
+
+function generateColors(){
+    colorContainerEls.forEach((colorContainerEl)=>{
+        const newColorCode = randomColor();
+        colorContainerEl.style.backgroundColor = "#" + newColorCode;
+        colorContainerEl.innerText = "#" + newColorCode;
+    })
+    
+}
+
+function randomColor(){
+    const chars = "0123456789abcdef";
+    
+    const colorCodeLength = 6
+    let colorCode = "";
+    for(let i = 0; i < colorCodeLength; i++){
+        const randomNum = Math.floor(Math.random() * chars.length);
+        console.log(randomNum);
+        colorCode += chars.substring(randomNum, randomNum +1);
+        
+
+    }
+    return colorCode;
+}
